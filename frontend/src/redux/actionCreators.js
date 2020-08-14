11aa2265c6ecf34645ecfbf35f83af897a2558f6
Nikita <ruseev.nikita@gmail.com>
2020-08-14 11:00:07 +0300

    basic redux

---
+++ frontend/src/redux/actionCreators.js
@@ -0,0 +1,62 @@
+import {
+  GET_PHASES_REQUEST, GET_PHASES_SUCCESS, GET_PHASES_FAILURE,
+  GET_WEEKS_REQUEST, GET_WEEKS_SUCCESS, GET_WEEKS_FAILURE,
+  GET_DAYS_REQUEST, GET_DAYS_SUCCESS, GET_DAYS_FAILURE
+} from './actionTypes'
+
+export function getPhasesRequest() {
+  return {
+    type: GET_PHASES_REQUEST,
+  }
+}
+export function getPhasesSuccess(name) {
+  return {
+    type: GET_PHASES_SUCCESS,
+    payload: {
+      name
+    }
+  }
+}
+export function getPhasesFailure() {
+  return {
+    type: GET_PHASES_FAILURE,
+  }
+}
+
+export function getWeeksRequest() {
+  return {
+    type: GET_WEEKS_REQUEST,
+  }
+}
+export function getWeeksSuccess(name) {
+  return {
+    type: GET_WEEKS_SUCCESS,
+    payload: {
+      name,
+    }
+  }
+}
+export function getWeeksFailure() {
+  return {
+    type: GET_WEEKS_FAILURE,
+  }
+}
+
+export function getDaysRequest() {
+  return {
+    type: GET_DAYS_REQUEST,
+  }
+}
+export function getDaysSuccess(name) {
+  return {
+    type: GET_DAYS_SUCCESS,
+    payload: {
+      name,
+    }
+  }
+}
+export function getDaysFailure() {
+  return {
+    type: GET_DAYS_FAILURE,
+  }
+}
---
+++ frontend/src/redux/reducers/IndexReducer.js
@@ -0,0 +1,62 @@
+import {
+  getPhasesRequest, getPhasesSuccess, getPhasesFailure,
+  getWeeksRequest, getWeeksSuccess, getWeeksFailure,
+  getDaysRequest, getDaysSuccess, getDaysFailure,
+} from '../actionCreators'
+
+const initialState = { data: [], loading: false }
+export default function ViewInfo(state = initialState, action) {
+  const { type, payload } = action
+  switch (type) {
+    case 'GET_PHASE_REQUEST':
+      return {
+        ...state,
+        loading: true
+      }
+    case 'GET_PHASE_SUCCESS':
+      return {
+        ...state,
+        loading: false,
+        data: payload.name
+      }
+    case 'GET_PHASE_FAILURE':
+      return {
+        ...state,
+        loading: false
+      }
+    case 'GET_WEEKS_REQUEST':
+      return {
+        ...state,
+        loading: true
+      }
+    case 'GET_WEEKS_SUCCESS':
+      return {
+        ...state,
+        loading: false,
+        data: payload.name
+      }
+    case 'GET_WEEKS_FAILURE':
+      return {
+        ...state,
+        loading: false
+      }
+    case 'GET_DAYS_REQUEST':
+      return {
+        ...state,
+        loading: true
+      }
+    case 'GET_DAYS_SUCCESS':
+      return {
+        ...state,
+        loading: false,
+        data: payload.name
+      }
+    case 'GET_DAYS_FAILURE':
+      return {
+        ...state,
+        loading: false
+      }
+    default:
+      return state
+  }
+}
