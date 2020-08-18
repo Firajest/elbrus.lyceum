ffbfe040e0d34986d1e76fd801c1335f5570ade8
Sergey <boma385@gmail.com>
2020-08-18 19:39:37 +0300

    smth

--- frontend/src/components/adminPage/chieftain.js
+++ frontend/src/components/adminPage/chieftain.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { newUserModalOn, clearMessages } from '../../redux/actionCreators'
+import { newUserModalOn, clearMessages } from '../../redux/actionCreators';
+import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom';
 
 function AdminPage() {
   const dispatch = useDispatch();
@@ -8,18 +9,22 @@ function AdminPage() {
   function createUser() {
     dispatch(clearMessages());
     dispatch(newUserModalOn());
-  }
+  };
 
   function showAllUsers() {
     
-  }
+  };
+
+  function addMaterials() {
+
+  };
 
   return (
     <>
       <ul>
         <li><button type="button" onClick={createUser}>Создать нового пользователя</button></li>
         <li><button type="button" onClick={showAllUsers}>Список пользователей</button></li>
-        <li><button type="button" onClick={createUser}>Добавить материал</button></li>
+        <li><button type="button" onClick={addMaterials}>Добавить материал</button></li>
       </ul>
     </>
   );
