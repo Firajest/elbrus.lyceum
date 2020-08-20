ecb2848c6382f3a23b454bb88b0e62f13ea54494
Tembot <maremshaov.t@yandex.ru>
2020-08-20 15:34:39 +0300

    small fixes

--- frontend/src/components/navbar/navbar.js
+++ frontend/src/components/navbar/navbar.js
@@ -89,7 +89,7 @@ function Navbar() {
             <Logout /> :
             <Button id="loginButton" className="dayButton" onClick={openLoginModal}>
               Login
-          </Button>
+            </Button>
           }
         </header>
       </div>
